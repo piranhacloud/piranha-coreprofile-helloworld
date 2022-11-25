6ba0da968c9eeba132673d140641c52c866053ed
Manfred Riem <mriem@manorrock.com>
2022-11-25 08:24:00 -0600

    Fixes issue #1 - Add template

---
+++ src/main/java/helloworld/HelloWorldApplication.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class HelloWorldApplication extends Application {
+}
